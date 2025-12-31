b52d2bc731d18f31510a1c4c9f427d6837e42a81
Pratik Gupta <spamacc4pratik@gmail.com>
2025-12-31 15:42:03 +0530

    Separate static frontend and serverless API for Vercel deployment

--- server.js
+++ server.js
@@ -359,14 +359,6 @@ app.get('/api/thumbnail/:type/:name', (req, res) => {
     res.redirect(`/api/thumbnail/${req.params.name}`);
 });
 
-// Serve static files from the React app build directory
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Catch-all handler: send back index.html for any non-API routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
 // For Vercel serverless, export the app instead of listening
 export default app;
 
